8858e4ad21ec430bd53b3d9538dc8bd4510d232f
jose.neto <jose.neto@red-gate.com>
2016-03-23 11:27:18 +0000

    Adds test for regular items update
    Tests if quality and sell in are reduced by 1

--- src/GildedRose.Tests/ExampleTests.cs
+++ src/GildedRose.Tests/ExampleTests.cs
@@ -1,3 +1,6 @@
+
+using System.Collections.Generic;
+using GildedRose.Console;
 using NUnit.Framework;
 
 namespace GildedRose.Tests
@@ -5,9 +8,17 @@ namespace GildedRose.Tests
     public class ExampleTests
     {
         [Test]
-        public void TestTheTruth()
+        public void RegularItemsDecreaseSellInAndQualityByOneOnUpdate()
         {
-            Assert.True(true);
+            var app = new Program();
+            IList<Item> Items;
+            Items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}
+            };
+            app.UpdateQuality(Items);
+            Assert.That(Items[0].Quality, Is.EqualTo(19));
+            Assert.That(Items[0].SellIn, Is.EqualTo(9));
         }
     }
 }
