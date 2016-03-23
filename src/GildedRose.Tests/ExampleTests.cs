88de60aedbd5d49a2532670d6eba398284f8ac14
jose.neto <jose.neto@red-gate.com>
2016-03-23 12:16:17 +0000

    Extracts method to build item to be tested

--- src/GildedRose.Tests/ExampleTests.cs
+++ src/GildedRose.Tests/ExampleTests.cs
@@ -7,37 +7,37 @@ namespace GildedRose.Tests
 {
     public class ExampleTests
     {
+        private IList<Item> buildTestItem(int sellIn, int quality)
+        {
+            return new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = sellIn, Quality = quality}
+            };
+        }
+
         [Test]
         public void RegularItemsDecreaseSellInAndQualityByOneOnUpdate()
         {
             var app = new Program();
-            IList<Item> Items;
-            Items = new List<Item>
-            {
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20}
-            };
-            app.UpdateQuality(Items);
-            Assert.That(Items[0].Quality, Is.EqualTo(19));
-            Assert.That(Items[0].SellIn, Is.EqualTo(9));
+            var items = buildTestItem(10, 20);
+            app.UpdateQuality(items);
+            Assert.That(items[0].SellIn, Is.EqualTo(9));
+            Assert.That(items[0].Quality, Is.EqualTo(19));
         }
 
         [Test]
         public void QualityIsNotNegativeForRegularItem()
         {
             var app = new Program();
-            IList<Item> Items;
-            Items = new List<Item>
-            {
-                new Item {Name = "+5 Dexterity Vest", SellIn = 3, Quality = 3}
-            };
+            var items = buildTestItem(3, 3);
             var count = 1;
             do
             {
-                app.UpdateQuality(Items);
+                app.UpdateQuality(items);
                 count++;
-            } while (count<=4)
-            ;
-            Assert.That(Items[0].Quality, Is.EqualTo(0));
+            } while (count <= 4);
+            Assert.That(items[0].Quality, Is.EqualTo(0));
         }
+
     }
 }
